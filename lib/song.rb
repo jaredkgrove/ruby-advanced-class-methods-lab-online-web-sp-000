d89533306e5ba245dd1151ad7689eddb290a53f6
Jared Grove <jaredkgrove@gmail.com>
2019-03-25 02:38:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    self.all.include?(name)
+    self.all.include?(name) ? self.find_by_name(name) : self.create_by_name(name)
   end
 
 end
